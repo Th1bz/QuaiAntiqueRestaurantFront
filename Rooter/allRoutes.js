ca6e116d9f701e46e40ef0fbb91b42e74e7bf516
Th1bz <thibaultwks@gmail.com>
2024-07-16 16:52:02 +0200

    galerie + page menu + first media querie

--- Rooter/allRoutes.js
+++ Rooter/allRoutes.js
@@ -1,7 +1,11 @@
 import Route from "./Route.js";
 
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/galerie", "Galerie", "/pages/galerie.html"),
+  new Route("/menu", "Menu", "/pages/menu.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
