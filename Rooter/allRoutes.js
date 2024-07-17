00957bedcab4b90c087c91b363e8e0589ce99bc4
Th1bz <thibaultwks@gmail.com>
2024-07-17 19:51:47 +0200

    gestion reservation

--- Rooter/allRoutes.js
+++ Rooter/allRoutes.js
@@ -14,6 +14,7 @@ export const allRoutes = [
     "/pages/auth/editPassword.html"
   ),
   new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+  new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
