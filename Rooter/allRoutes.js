a3a1bb1fad7658dff672a6c5bfbf2a42450f118f
Th1bz <thibaultwks@gmail.com>
2024-07-17 13:47:49 +0200

    Ajout page account et editPassword

--- Rooter/allRoutes.js
+++ Rooter/allRoutes.js
@@ -5,8 +5,14 @@ export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html"),
   new Route("/menu", "Menu", "/pages/menu.html"),
-  new Route("/signin", "Connexion", "/pages/signin.html"),
-  new Route("/signup", "Inscription", "/pages/signup.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
+  new Route(
+    "/editPassword",
+    "Modifiez votre mot de passe",
+    "/pages/auth/editPassword.html"
+  ),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
