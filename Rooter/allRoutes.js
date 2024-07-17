78cf29b1f74a595893cf31ddd3ea845ac9b32dcd
Th1bz <thibaultwks@gmail.com>
2024-07-17 13:14:34 +0200

    Ajout pages signin et signup

--- Rooter/allRoutes.js
+++ Rooter/allRoutes.js
@@ -5,6 +5,8 @@ export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html"),
   new Route("/menu", "Menu", "/pages/menu.html"),
+  new Route("/signin", "Connexion", "/pages/signin.html"),
+  new Route("/signup", "Inscription", "/pages/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
